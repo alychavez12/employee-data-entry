91137abdb511e7562abc3801163c8bbb2452b481
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-01-28 16:06:39 -0500

    wip: creates a show and index route working

--- controllers/employees.js
+++ controllers/employees.js
@@ -9,10 +9,20 @@ const Employee = require('../models/employees');
 // Seed Route
 
 // Index
+router.get('/employees', (req, res) => {
+    Employee.find({}, (err, allEmployees) => {
+        res.render('index.ejs', {
+            employees: allEmployees,
+            title: 'index page'
+        });
+    });
+});
 
 // New
 router.get('/employees/new', (req, res) => {
-    res.render('new.ejs');
+    res.render('new.ejs', {
+        title:'Add a new employee'
+    });
 });
 
 // Delete
@@ -22,12 +32,22 @@ router.get('/employees/new', (req, res) => {
 // Create
 router.post('/employees', (req, res) => {
     Employee.create(req.body, (err, createdEmployee) => {
-       res.send(createdEmployee); 
+       res.redirect('/employees', {
+        title: 'create route'
+       }); // redirect to index page
     });
 });
 
 // Edit
 
 // Show
+router.get('/employees/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, foundEmployee) => {
+        res.render('show.ejs', {
+            title: 'show page'
+        });
+
+    });
+});
 
 module.exports = router;
