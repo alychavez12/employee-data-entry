f9e6c12948d54e80de7da4430e161621be959794
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-01-28 13:21:20 -0500

    wip: require expess

---
+++ controllers/employees.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const Employee = require('../models/employees');
+
+
+module.exports = router;
