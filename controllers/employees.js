666757ebe5f68b84a8c4a4c3b1df4758904b7561
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-01-28 15:20:19 -0500

    wip: adds a create and a new route working

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data');
+// const data = require('../data');
+const employeeRouter = require('../models/employees');
 const Employee = require('../models/employees');
 
+// routes INDUCES
+
+// Seed Route
+
+// Index
+
+// New
+router.get('/employees/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+// Delete
+
+// Update
+
+// Create
+router.post('/employees', (req, res) => {
+    Employee.create(req.body, (err, createdEmployee) => {
+       res.send(createdEmployee); 
+    });
+});
+
+// Edit
+
+// Show
 
 module.exports = router;
