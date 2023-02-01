b45b3439c7f5e189dc484bbaae433fc4ba09106b
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-01-31 19:41:33 -0500

    wip: adds a new Schema model

--- models/employees.js
+++ models/employees.js
@@ -2,9 +2,14 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
 const employeeSchema = new Schema({
+    idnumber: Number,
     firstname: String,
     lastname: String,
+    dob: String,
     gender: String,
+    postcode: String,
+    emailaddress: String,
+    phonenumber: String,
     position: String
 
 });
