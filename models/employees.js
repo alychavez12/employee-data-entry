147b23b9e5173989380d39d827eeb5cd5a25ab28
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-02-04 11:15:40 -0500

    wip: adds object structure

--- models/employees.js
+++ models/employees.js
@@ -1,17 +1,67 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const employeeSchema = new Schema({
-    idnumber: {type: Number, required: true},
-    firstname: {type: String, required: true},
-    lastname: {type: String, required: true},
-    dob: {type: String, required: true},
-    gender: {type: String, required: true},
-    postcode: {type: String, required: true},
-    emailaddress: {type: String, required: true},
-    phonenumber: {type: String, required: true},
-    position: {type: String, required: true},
+  idnumber: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  dob: {
+    type: Date,
+    required: true,
+    trim: true
+  },
+  gender: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  postcode: {
+    type: String,
+    required: true,
+    trim: true,
+    min: [5, 'Just 5 digits'],
+    max: [5, 'max 5 digits']
+  },
+  emailaddress: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  phonenumber: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  position: {
+    type: String,
+    required: true,
+    trim: true
+  },
 
 });
+
+
+// capitalize the first letter of name and last name
+employeeSchema.pre('save', function (next) {
+  const words = this.firstname.split(' ')
+  this.firstname = words
+    .map((w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())
+    .join(' ')
+  next()
+})
+
 
 module.exports = mongoose.model('Employee', employeeSchema);
